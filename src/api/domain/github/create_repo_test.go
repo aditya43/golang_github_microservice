0724dabe9c765c6946567f73ae1e325bf7f9f50f
Aditya Hajare <aditya43@gmail.com>
2021-05-13 12:49:06 +0530

    Test JSON unmarshal into request struct

--- src/api/domain/github/create_repo_test.go
+++ src/api/domain/github/create_repo_test.go
@@ -23,5 +23,10 @@ func TestCreateRepoRequestAsJson(t *testing.T) {
 	assert.Nil(t, err)
 	assert.NotNil(t, bytes)
 	// t.Log(string(bytes))
-	assert.EqualValues(t, `{"name":"Aditya Test Repo","description":"This is a test git repository","homepage":"https://aditya.com","private":true,"has_issues":true,"has_projects":true,"has_wiki":true}`, string(bytes))
+	// assert.EqualValues(t, `{"name":"Aditya Test Repo","description":"This is a test git repository","homepage":"https://aditya.com","private":true,"has_issues":true,"has_projects":true,"has_wiki":true}`, string(bytes))
+
+	var target CreateRepoRequest
+	// Unmarshal takes input byte array (JSON) and a pointer to struct we want to populate with JSON fields
+	err = json.Unmarshal(bytes, &target)
+	assert.Nil(t, err)
 }
