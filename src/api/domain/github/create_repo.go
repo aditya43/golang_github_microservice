2bb9507927a22d1d0d118d4c0479cde19c3b136d
Aditya Hajare <aditya43@gmail.com>
2021-05-13 12:39:21 +0530

    Add JSON structure

--- src/api/domain/github/create_repo.go
+++ src/api/domain/github/create_repo.go
@@ -1,11 +1,11 @@
 package github
 
 type CreateRepoRequest struct {
-	Name        string
-	Description string
-	Homepage    string
-	Private     bool
-	HasIssues   bool
-	HasProjects bool
-	HasWiki     bool
+	Name        string `json:"name"`
+	Description string `json:"description"`
+	Homepage    string `json:"homepage"`
+	Private     bool   `json:"private"`
+	HasIssues   bool   `json:"has_issues"`
+	HasProjects bool   `json:"has_projects"`
+	HasWiki     bool   `json:"has_wiki"`
 }
