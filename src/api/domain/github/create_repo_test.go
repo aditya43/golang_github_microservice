096a83e8eccc8dd93702b8c3035a0bb47afccddf
Aditya Hajare <aditya43@gmail.com>
2021-05-13 14:14:37 +0530

    Refactor test cases

--- src/api/domain/github/create_repo_test.go
+++ src/api/domain/github/create_repo_test.go
@@ -19,15 +19,15 @@ func TestCreateRepoRequestAsJson(t *testing.T) {
 	}
 
 	// Marshal takes an input interface and attempts to create a valid JSON string
-	bytes, err := json.Marshal(request)
+	jsonBytes, err := json.Marshal(request)
 	assert.Nil(t, err)
-	assert.NotNil(t, bytes)
-	// t.Log(string(bytes))
-	// assert.EqualValues(t, `{"name":"Aditya Test Repo","description":"This is a test git repository","homepage":"https://aditya.com","private":true,"has_issues":true,"has_projects":true,"has_wiki":true}`, string(bytes))
+	assert.NotNil(t, jsonBytes)
+	// t.Log(string(jsonBytes))
+	// assert.EqualValues(t, `{"name":"Aditya Test Repo","description":"This is a test git repository","homepage":"https://aditya.com","private":true,"has_issues":true,"has_projects":true,"has_wiki":true}`, string(jsonBytes))
 
 	var target CreateRepoRequest
 	// Unmarshal takes input byte array (JSON) and a pointer to struct we want to populate with JSON fields
-	err = json.Unmarshal(bytes, &target)
+	err = json.Unmarshal(jsonBytes, &target)
 	assert.Nil(t, err)
 
 	assert.EqualValues(t, request.Name, target.Name)
