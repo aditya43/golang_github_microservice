4f688f0929044cf8fe1305432400c37b48e50484
Aditya Hajare <aditya43@gmail.com>
2021-05-14 11:20:59 +0530

    Repo controller

---
+++ src/api/controllers/repositories/repositories_controller.go
@@ -0,0 +1,44 @@
+package repositories
+
+import (
+	"net/http"
+
+	"github.com/aditya43/golang_github_microservice/src/api/domain/repositories"
+	"github.com/aditya43/golang_github_microservice/src/api/services"
+	"github.com/aditya43/golang_github_microservice/src/api/utils/errors"
+	"github.com/gin-gonic/gin"
+)
+
+func CreateRepo(c *gin.Context) {
+	var request repositories.CreateRepoRequest
+	if err := c.ShouldBindJSON(&request); err != nil {
+		apiErr := errors.NewBadRequestError("invalid json body")
+		c.JSON(apiErr.Status(), apiErr)
+		return
+	}
+
+	clientId := c.GetHeader("X-Client-Id")
+
+	result, err := services.RepositoryService.CreateRepo(clientId, request)
+	if err != nil {
+		c.JSON(err.Status(), err)
+		return
+	}
+	c.JSON(http.StatusCreated, result)
+}
+
+func CreateRepos(c *gin.Context) {
+	var request []repositories.CreateRepoRequest
+	if err := c.ShouldBindJSON(&request); err != nil {
+		apiErr := errors.NewBadRequestError("invalid json body")
+		c.JSON(apiErr.Status(), apiErr)
+		return
+	}
+
+	result, err := services.RepositoryService.CreateRepos(request)
+	if err != nil {
+		c.JSON(err.Status(), err)
+		return
+	}
+	c.JSON(result.StatusCode, result)
+}
